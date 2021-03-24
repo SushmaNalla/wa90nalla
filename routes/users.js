80d78fd640d464ee9540c5eb9646345ccb5965c6
SushmaNalla <77800401+SushmaNalla@users.noreply.github.com>
2021-03-24 01:26:35 -0500

    user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var a = 0;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  a = a + 1;
+  res.send('User accesses are: ' + a);  
 });
 
 module.exports = router;
