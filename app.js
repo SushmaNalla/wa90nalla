93bf6ba3be4b8a0c5c4e16323f55bfc60e0ff9e6
SushmaNalla <77800401+SushmaNalla@users.noreply.github.com>
2021-03-24 16:32:30 -0500

    bonus

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/compute', computeRouter);
+app.use('/computation', compute.Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
