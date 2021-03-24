ba3392e912d49993c7519ba85ee6696734b00586
SushmaNalla <77800401+SushmaNalla@users.noreply.github.com>
2021-03-24 16:51:51 -0500

    bonus

--- routes/compute.js
+++ routes/compute.js
@@ -7,8 +7,8 @@ router.get('/',(req,res)=>{
     let data=req.query.x
     console.log(data)
     res.render('compute',{
-        randSolution:`atan2 applied to ${randomNumber} is ${Math.atan2(randomNumber)}`,
-        querySolution:` atan2 applied to ${data} is ${Math.atan2(data)}`
+        randSolution:`cbrt applied to ${randomNumber} is ${Math.cbrt(randomNumber)}`,
+        querySolution:` cbrt applied to ${data} is ${Math.cbrt(data)}`
     })
 })
 
